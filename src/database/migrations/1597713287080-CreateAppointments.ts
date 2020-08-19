618125a7533d3aec61ad9387946756c5ef208921
Gabrielncosta <gabrielncosta208@gmail.com>
2020-08-18 22:06:22 -0300

    Appointment Routes

--- src/database/migrations/1597713287080-CreateAppointments.ts
+++ src/database/migrations/1597713287080-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1597713287080
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,4 @@
 import { Router } from 'express';
-
-const routes = Router();
-
-routes.post('/', (req, res) => {
-  const { name, email } = req.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return res.json(user);
-});
+import routes from './appointments.routes';
 
 export default routes;
