09cb7a300c5cd5c45314e93db10245d7f740403e
Gabrielncosta <gabrielncosta208@gmail.com>
2020-08-19 08:33:15 -0300

    refac: add created_at and updated_at to appointments model and migration

--- src/database/migrations/1597713287080-CreateAppointments.ts
+++ src/database/migrations/1597713287080-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1597713287080
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
