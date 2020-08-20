1e1b0bf714aa981b5fdf8f96dc18d264141189c9
Gabrielncosta <gabrielncosta208@gmail.com>
2020-08-20 19:12:26 -0300

    refac: appointment changes to reflect relationship with user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,10 @@
 import { Router } from 'express';
-import routes from './appointments.routes';
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
