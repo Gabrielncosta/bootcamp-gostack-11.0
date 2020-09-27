4f83e3485c367d49b365d66e2287f0f4204082f3
Gabrielncosta <gabrielncosta208@gmail.com>
2020-09-27 16:35:38 -0300

    test: MailService tests and providers

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -10,7 +10,7 @@ import '@shared/infra/typeorm';
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
